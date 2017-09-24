9fc7605e1a1757857ae560f0608f5517dc44d9ae
RodrigoDePool <rodrigodp05@gmail.com>
2017-09-24 13:50:22 +0200

    "a"

--- practica1/parte1/codigo/practica1.c
+++ practica1/parte1/codigo/practica1.c
@@ -1,17 +1,3 @@
-/***************************************************************************
- EjemploPcapNext.c
- Muestra el tiempo de llegada de los primeros 500 paquetes a la interface eth0
-y los vuelca a traza (¿correctamente?) nueva con tiempo actual
-
- Compila: gcc -Wall -o EjemploPcapNextEx EjemploPcapNextEx.c -lpcap
- Autor: Jose Luis Garcia Dorado
- 2017 EPS-UAM
-***************************************************************************/
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
@@ -21,85 +7,187 @@ y los vuelca a traza (¿correctamente?) nueva con tiempo actual
 #include <linux/tcp.h>
 #include <signal.h>
 #include <time.h>
-#define OK 0
-#define ERROR 1
+#include "practica1.h"
 
-#define ETH_FRAME_MAX 1514	// Tamanio maximo trama ethernet
+/*******
+	ESQUEMA MENTAL;
+	Caso live:
+	descr1 = open_live;
+	descr2 = open dead; descr3 = dump_open(descr2); <--- PARA VOLCAR TRAZA
+	Caso offline:
+	descr1 = open_offline;
 
+	En ambos casos:
+	Empiezo a leer y a imprimir con pcapNext.
+	Cuento numero de paquetes
+
+	Lo que diferencia los dos casos es cuando paro de leer y muestro num paquetes
+	Caso live: sigint --> POR TANTO, numPaquetes global
+	Caso offline: pcapNext == -2 :D
+
+******/
+
+
+/* Global vars */
 pcap_t *descr=NULL,*descr2=NULL;
 pcap_dumper_t *pdumper=NULL;
+int nPaquetes = 0; /* TODO:  necesita ser global? */
 
+/*
+	Realiza control de argumentos de entrada.
+	Prepara descr, descr2 y pdumper para leer trafico segun los argumentos de entrada.
+	Devuelve nBytes a escribir de cada paquete.
+*/
+int ini(int argc, char **argv);
+
+/*
+	Se encarga de cerrar los descriptores globales
+*/
+void cerrar();
+
+/*
+	Funcion que anyade dos dias  a la fecha de llegada
+	de la cabacera de un paquete
+*/
+void sumar_dos(struct pcap_pkthdr *cabecera);
+
+/*
+	Funcion que imprime el numero de bytes solicitados de un paquete.
+	Si el paquete tiene menos bytes que los solicitados, se imprime
+	todo el paquete.
+*/
+void imprime_paquete(uint8_t *paquete,struct pcap_pkthdr *cabecera, long int nbytes);
+
+/* Handle : cuando Ctrl-C, imprime nPaquetes*/
 void handle(int nsignal){
-	printf("Control C pulsado\n");
-	if(descr)
-		pcap_close(descr);
-	if(descr2)
-		pcap_close(descr2);
-	if(pdumper)
-		pcap_dump_close(pdumper);
+	printf("%d paquetes fueron recibidos.\n", nPaquetes);
+	cerrar();
 	exit(OK);
- }
-
-int main(int argc, char **argv)
-{
-	int retorno=0,contador=0;
-	char errbuf[PCAP_ERRBUF_SIZE];
-	uint8_t *paquete=NULL;
-	struct pcap_pkthdr *cabecera=NULL;
-	char file_name[256];
-	struct timeval time;
-
-	if(signal(SIGINT,handle)==SIG_ERR){
-		printf("Error: Fallo al capturar la senal SIGINT.\n");
-		exit(ERROR);
-	}	
-		//Apertura de interface
-   	if ((descr = pcap_open_live("enp4s0",9,0,100, errbuf)) == NULL){
-		printf("Error: pcap_open_live(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
-		exit(ERROR);
-	}
-		//Volcado de traza
-	descr2=pcap_open_dead(DLT_EN10MB,ETH_FRAME_MAX);
-	if (!descr2){
-		printf("Error al abrir el dump.\n");
-		pcap_close(descr);
-		exit(ERROR);
-	}
-	gettimeofday(&time,NULL);
-	sprintf(file_name,"eth0.%lld.pcap",(long long)time.tv_sec);
-	pdumper=pcap_dump_open(descr2,file_name);
-	if(!pdumper){
-		printf("Error al abrir el dumper: %s, %s %d.\n",pcap_geterr(descr2),__FILE__,__LINE__);
-		pcap_close(descr);
-		pcap_close(descr2);
-	}
-
-
-	while (contador<500){
-		retorno = pcap_next_ex(descr,&cabecera,(const u_char **)&paquete);
-		if(retorno == -1){ 		//En caso de error
-			printf("Error al capturar un paquete %s, %s %d.\n",pcap_geterr(descr),__FILE__,__LINE__);
-			pcap_close(descr);
-			pcap_close(descr2);
-			pcap_dump_close(pdumper);
-			exit(ERROR);
-		}
-		else if(retorno == 0){
-			continue;
-		}
-		else if(retorno==-2){
-			break;
-		}
-			//En otro caso
-		contador++;
-		printf("Nuevo paquete capturado a las %s\n",ctime((const time_t*)&(cabecera->ts.tv_sec)));
-		if(pdumper){
-			pcap_dump((uint8_t *)pdumper,cabecera,paquete);
-		}
-	}
-	pcap_close(descr);
-	pcap_close(descr2);
-	pcap_dump_close(pdumper);
-	return OK;
 }
 
+int main(int argc, char **argv){
+	long int nBytes = 0;
+	int retorno = 0;
+	uint8_t *paquete=NULL;
+	struct pcap_pkthdr *cabecera=NULL;
+
+	nBytes = ini(argc, argv);
+	if(nBytes == ERROR){
+		return ERROR;
+	}
+
+	while(retorno >= 0){
+		retorno = pcap_next_ex(descr,&cabecera,(const u_char **)&paquete);
+		if(retorno > 0){
+			nPaquetes++;
+			/*Volcado*/
+			if(argc == 2){
+				sumar_dos(cabecera);
+				pcap_dump((uint8_t *)pdumper,cabecera,paquete);
+			}
+			printf("Paquete %d:\n",nPaquetes);
+			imprime_paquete(paquete,cabecera,nBytes);
+		}
+	}
+
+	/*Caso de error*/
+	if(retorno == -1){
+		printf("Error al capturar un paquete %s, %s %d.\n",pcap_geterr(descr),__FILE__,__LINE__);
+		cerrar();
+		exit(ERROR);
+	}
+	/*Caso de que no habia mas paquetes que leer*/
+	printf("%d paquetes fueron leidos.\n", nPaquetes);
+	cerrar();
+	return OK;
+
+
+}
+
+int ini(int argc, char **argv){
+	char errbuf[PCAP_ERRBUF_SIZE];
+	char file_name[256];
+	struct timeval time;
+	int nBytes;
+
+	/*Analizamos args entrada*/
+	if(argc < 2 || argc > 3){
+		printf("Argumentos invalidos. Si desea:\n");
+		printf("1)Capturar de interfaz: introduce nº de bytes a leer por paquete.\n");
+		printf("2)Analizar traza: introduce nº de bytes a leer por paquete + traza a analizar\n");
+		return ERROR;
+	}
+
+	/*EN AMBOS CASOS ??? O SOLO CUANDO ES LIVE ???*/
+	if(signal(SIGINT,handle)==SIG_ERR){
+		printf("Error: Fallo al capturar la senal SIGINT.\n");
+		return ERROR;
+	}
+
+	nBytes = strtol(argv[1], NULL, 10);
+
+	if(nBytes <= 0){
+		printf("Error: el primer argumento debe ser un entero > 1.\n");
+		return ERROR;
+	}
+
+	 /* Distinguimos abrir interfaz/traza */
+	if(argc == 2){
+		/*INTERFAZ LUCIA enp4s0*/
+		descr = pcap_open_live("wlp3s0",ETH_FRAME_MAX,0,100, errbuf);
+		if (descr == NULL){
+			printf("Error: pcap_open_live(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
+			return ERROR;
+		}
+
+		descr2=pcap_open_dead(DLT_EN10MB,ETH_FRAME_MAX);
+		if (descr2 == NULL){
+			printf("Error al abrir el dump.\n");
+			pcap_close(descr);
+			exit(ERROR);
+		}
+		gettimeofday(&time,NULL);
+		sprintf(file_name,"eth0.%lld.pcap",(long long)time.tv_sec);
+		pdumper=pcap_dump_open(descr2,file_name);
+		if(!pdumper){
+			printf("Error al abrir el dumper: %s, %s %d.\n",pcap_geterr(descr2),__FILE__,__LINE__);
+			pcap_close(descr);
+			pcap_close(descr2);
+			return ERROR;
+		}
+	}else{
+		descr = pcap_open_offline(argv[2], errbuf);
+		if( descr == NULL){
+			printf("Error: pcap_open_offline(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
+			return ERROR;
+		}
+	}
+	return nBytes;
+}
+
+void cerrar(){
+	if( descr != NULL )
+		pcap_close(descr);
+	if( descr2 != NULL )
+		pcap_close(descr2);
+	if( pdumper != NULL )
+		pcap_dump_close(pdumper);
+}
+
+
+void sumar_dos(struct pcap_pkthdr *cabecera){
+	if(cabecera == NULL) return;
+	cabecera->ts.tv_sec = cabecera->ts.tv_sec + TWODAYS_SECS;
+}
+
+
+void imprime_paquete(uint8_t *paquete,struct pcap_pkthdr *cabecera, long int nbytes){
+	int min = nbytes;
+	int i;
+	if(paquete == NULL || cabecera == NULL) return;
+	/*Minimo entre bytes totales y solicitados*/
+	if(cabecera->caplen < min) min = cabecera->caplen;
+	for(i=0; i<min ; i++)
+		printf("%02x ",paquete[i]);
+	printf("\n\n");
+}
--- practica1/parte1/codigo/practica1.h
+++ practica1/parte1/codigo/practica1.h
@@ -3,7 +3,7 @@
 
 #define OK 0
 #define ERROR -1
-
+#define TWODAYS_SECS 172800
 #define ETH_FRAME_MAX 2048// Tamanio maximo trama ethernet
 
 #endif
