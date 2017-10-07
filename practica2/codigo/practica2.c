536a3946aa62d5e7f2578572ccd7fa9a5f5347d7
RodrigoDePool <rodrigodp05@gmail.com>
2017-10-07 21:00:55 +0200

    "a"

--- practica2/codigo/practica2.c
+++ practica2/codigo/practica2.c
@@ -12,13 +12,27 @@
 #include <getopt.h>
 #include <inttypes.h>
 
-
+/*Definicion de constantes *************************************************/
+#define ETH_ALEN      6      /* Tamanio de la direccion ethernet           */
+#define ETH_HLEN      14     /* Tamanio de la cabecera ethernet            */
+#define ETH_TLEN      2      /* Tamanio del campo tipo ethernet            */
+#define ETH_FRAME_MAX 1514   /* Tamanio maximo la trama ethernet (sin CRC) */
+#define ETH_FRAME_MIN 60     /* Tamanio minimo la trama ethernet (sin CRC) */
+#define ETH_DATA_MAX  (ETH_FRAME_MAX - ETH_HLEN) /* Tamano maximo y minimo de los datos de una trama ethernet*/
+#define ETH_DATA_MIN  (ETH_FRAME_MIN - ETH_HLEN)
+#define IP_ALEN 4			/* Tamanio de la direccion IP					*/
+#define OK 0
+#define ERROR 1
+#define PACK_READ 1
+#define PACK_ERR -1
+#define TRACE_END -2
+#define NO_FILTER 0
 
 
 pcap_t *descr = NULL;
 uint64_t contador = 0;
-uint8_t ipsrc_filter[IP_ALEN] = "";
-uint8_t ipdst_filter[IP_ALEN] = "";
+uint8_t ipsrc_filter[IP_ALEN] = {0};
+uint8_t ipdst_filter[IP_ALEN] = {0};
 uint16_t sport_filter= NO_FILTER;
 uint16_t dport_filter = NO_FILTER;
 
@@ -56,6 +70,27 @@ int init(int argc, char **argv);
 */
 void printFiltros();
 
+/*
+*	Se encarga de imprimir la informacion referente al paquete capturado
+*/
+void analizar_paquete(const struct pcap_pkthdr *hdr, const uint8_t *pack);
+
+/*
+*	Analiza el paquete a nivel 3 (llamada internamente por analizar paquete)
+*	Luego si el paquete pasa los filtros se llama a analizar el nivel 4
+*/
+void analizar_nivel3(const struct pcap_pkthdr *hdr, const uint8_t *pack);
+
+/*
+*	Analiza el paquete TCP (llamada internamente por analizar_nivel3)
+*/
+void analizar_TCP(const struct pcap_pkthdr *hdr, const uint8_t *pack);
+
+/*
+*	Analiza el paquete UDP(llamada internamente por analizar_nivel3)
+*/
+void analizar_UDP(const struct pcap_pkthdr *hdr, const uint8_t *pack);
+
 int main(int argc, char **argv)
 {
 	uint8_t *pack = NULL;
@@ -71,13 +106,13 @@ int main(int argc, char **argv)
 		mensajeFormatoError();
 		exit(ERROR);
 	}
-	
+
 	if(init(argc,argv) == ERROR){
 		exit(ERROR);
 	}
 
 	printFiltros();
-	
+
 
 	do {
 		retorno = pcap_next_ex(descr, &hdr, (const u_char **)&pack);
@@ -103,7 +138,7 @@ int main(int argc, char **argv)
 
 
 int init(int argc, char **argv){
-	int i;
+	int i, aux;
 	char errbuf[PCAP_ERRBUF_SIZE];
 
 	if(argc <= 1 || argv == NULL) return ERROR;
@@ -137,9 +172,19 @@ int init(int argc, char **argv){
 				return ERROR;
 			}
 		}else if( strcmp(argv[i],"-ipo") == 0 ){
-			sscanf(argv[i+1]," %"SCNu8".%"SCNu8".%"SCNu8".%"SCNu8"",ipsrc_filter,ipsrc_filter + 1,ipsrc_filter + 2,ipsrc_filter + 3);
+			aux = sscanf(argv[i+1]," %"SCNu8".%"SCNu8".%"SCNu8".%"SCNu8"",ipsrc_filter,ipsrc_filter + 1,ipsrc_filter + 2,ipsrc_filter + 3);
+			if(aux != 4){
+				printf("Error en el filtro de ip origen.\n");
+				if(descr != NULL) pcap_close(descr);
+				return ERROR;
+			}
 		}else if( strcmp(argv[i],"-ipd") == 0 ){
-			sscanf(argv[i+1]," %"SCNu8".%"SCNu8".%"SCNu8".%"SCNu8"",ipdst_filter,ipdst_filter + 1,ipdst_filter + 2,ipdst_filter + 3);
+			aux = sscanf(argv[i+1]," %"SCNu8".%"SCNu8".%"SCNu8".%"SCNu8"",ipdst_filter,ipdst_filter + 1,ipdst_filter + 2,ipdst_filter + 3);
+			if(aux != 4){
+				printf("Error en el filtro de ip destino.\n");
+				if(descr != NULL) pcap_close(descr);
+				return ERROR;
+			}
 		}else if( strcmp(argv[i],"-po") == 0 ){
 			sport_filter = atoi(argv[i+1]);
 			if(sport_filter == 0){
@@ -176,43 +221,198 @@ void printFiltros(){
 	if (dport_filter != NO_FILTER) {
 		printf("pd_filtro=%"PRIu16"\t", dport_filter);
 	}
-	if( strcmp(ipsrc_filter,"") != 0 ){
-		printf("ipsrc_filter:%"PRIu8".%"PRIu8".%"PRIu8".%"PRIu8"\t", ipsrc_filter[0], ipsrc_filter[1], ipsrc_filter[2], ipsrc_filter[3]);	
+	if( ipsrc_filter[0] != 0 ){
+		printf("ipsrc_filter:%"PRIu8".%"PRIu8".%"PRIu8".%"PRIu8"\t", ipsrc_filter[0], ipsrc_filter[1], ipsrc_filter[2], ipsrc_filter[3]);
 	}
-	if( strcmp(ipdst_filter,"") != 0 ){
-		printf("ipdst_filter:%"PRIu8".%"PRIu8".%"PRIu8".%"PRIu8"\t", ipdst_filter[0], ipdst_filter[1], ipdst_filter[2], ipdst_filter[3]);	
+	if( ipdst_filter[0] != 0 ){
+		printf("ipdst_filter:%"PRIu8".%"PRIu8".%"PRIu8".%"PRIu8"\t", ipdst_filter[0], ipdst_filter[1], ipdst_filter[2], ipdst_filter[3]);
 	}
 	printf("\n\n");
 }
 
 void analizar_paquete(const struct pcap_pkthdr *hdr, const uint8_t *pack)
 {
-	printf("Nuevo paquete capturado el %s\n", ctime((const time_t *) & (hdr->ts.tv_sec)));
+	int i;
+	uint16_t *aux_paquete;
+	uint16_t aux;
 
-	int i = 0;
-	printf("Direccion ETH destino= ");
+	printf("Paquete numero %"PRIu64" capturado el %s\n", contador,ctime((const time_t *) & (hdr->ts.tv_sec)));
+	printf("Direccion ETH destino = ");
 	printf("%02X", pack[0]);
-
 	for (i = 1; i < ETH_ALEN; i++) {
 		printf(":%02X", pack[i]);
 	}
-
 	printf("\n");
 	pack += ETH_ALEN;
 
 	printf("Direccion ETH origen = ");
 	printf("%02X", pack[0]);
-
 	for (i = 1; i < ETH_ALEN; i++) {
 		printf(":%02X", pack[i]);
 	}
-
 	printf("\n");
+	pack+=ETH_ALEN;
 
-	//pack+=ETH_ALEN;
-	// .....
-	// .....
-	// .....
-
+	aux_paquete = (uint16_t *) pack;
+	aux = ntohs(aux_paquete[0]);
+	if(aux == 2048){
+		printf("Tipo ethernet = IPv4\n");
+		pack+=ETH_TLEN;
+		analizar_nivel3(hdr,pack);
+	}else{
+		printf("Tipo ethernet = 0x%04x\n",aux);
+		printf("El protocolo no es el esperado. Por tanto, no se imprimiran los siguientes niveles.\n");
+	}
 	printf("\n\n");
+}
+
+void analizar_nivel3(const struct pcap_pkthdr *hdr, const uint8_t *pack){
+	uint8_t aux8;
+	uint8_t *pack_aux;
+	uint16_t *paux;
+	uint16_t aux16;
+	int flag;/*flag para avisar que no se continua al siguiente nivel*/
+	int prot;/*0 para analizar udp, 1 para analizar tcp*/
+
+	printf("  Cabecera IPv4:\n");
+	/*Version*/
+	aux8 = 240;/*11110000*/
+	aux8 = aux8 & pack[0]; /*Tenemos campo version en los 4 primeros bits*/
+	aux8 = aux8 >> 4;
+	printf("Version = %"PRIu8"\n",aux8);
+
+	/*Longitud cabecera*/
+	aux8 = 15; /*00001111 en binario*/
+	aux8 = aux8 & pack[0]; /*Eliminamos asi el campo version, tenemos IHL*/
+	aux8 = (aux8 & pack[0])*4; /* IHL*4 tamanio en bytes de cabecera ip*/
+	printf("Longitud de cabecera ip en bytes = %"PRIu8"\n",aux8);
+	pack_aux = (uint8_t *)pack + aux8;/*Nos guardamos el final de la cabecera ip*/
+
+	pack += 2;/*Posicion antes de la longitud*/
+	/*Longitud*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Longitud total = %"PRIu16"\n",aux16);
+
+	pack += 4; /*Posicion antes de flags*/
+	/*Posicion*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	aux16 = aux16 & 8191;/*0001111111111111 and aux16, resultado = posicionamiento*/
+	aux16 = aux16*8; /*Posicionamiento por unidad de 64 bits a posci por byte*/
+	printf("Posicionamiento = %"PRIu16"\n",aux16);
+	if(aux16 != 0){
+		flag = 1;
+		printf(" No se mostrara el siguiente nivel por no ser el primer fragmento\n");
+	}
+
+	pack += 2;/*Posicion antes de tiempo de vida*/
+	/*Tiempo de vida*/
+	printf("Tiempo de vida = %"PRIu8"\n",pack[0]);
+
+	pack += 1;/*Posicion antes de protocolo*/
+	/*Protocolo*/
+	if(pack[0] == 6){
+		printf("Protocolo = TCP\n");
+		prot = 1;
+	}else if(pack[0] == 17){
+		printf("Protocolo = UDP\n");
+		prot = 0;
+	}else{
+		printf("Protocolo = %"PRIu8"\n",pack[0]);
+		printf(" No se mostrara el siguiente nivel porque el protocolo no es ni TCP ni UDP\n");
+		flag = 1;
+	}
+
+	pack += 3;/*Antes de ip origen*/
+	/*IP origen*/
+	/*crear uint de 32, hacer un ntohl */
+	/*utilizar uint8 y con el and correspondiente hacer byte a byte*/
+	/*El filtro tambien puede hacerse byte a byte*/
+	/*TODO:FILTRO Y CODIGO*/
+
+	pack += 4;
+	/*IP destino*/
+	/*TODO:FILTRO Y CODIGO*/
+
+
+	if(flag == 1){
+		return;
+	}else if(prot == 0){
+		analizar_UDP(hdr,pack_aux);
+	}else{
+		analizar_TCP(hdr,pack_aux);
+	}
+}
+
+void analizar_TCP(const struct pcap_pkthdr *hdr, const uint8_t *pack){
+	uint16_t *paux;
+	uint16_t aux16;
+	uint8_t aux8;
+	uint8_t syn, ack;
+
+	printf("  Cabecera TCP:\n");
+
+	/*Puerto origen*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Puerto origen = %"PRIu16"\n", aux16);
+	if(sport_filter != 0 && aux16 != sport_filter){
+		printf("El puerto origen no pasa el filtro, no se mostraran mas campos\n");
+		return;
+	}
+
+	pack += 2;/*Antes de puerto destino*/
+	/*Puerto destino*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Puerto destino = %"PRIu16"\n", aux16);
+	if(dport_filter != 0 && aux16 != dport_filter){
+		printf("El puerto destino no pasa el filtro, no se mostraran mas campos\n");
+		return;
+	}
+
+	pack += 11;/*Ultimos dos bits de reservado y flags*/
+	/*Flags*/
+	syn = 2 & pack[0];/* 00000010 and pack[0] = flag de syn*/
+	if(syn == 0) printf("Flag SYN = 0\n");
+	else printf("Flag SYN = 1\n");
+	ack = 16 & pack[0]; /*00010000 and pack[0] = flag de ack*/
+	if(ack == 0 ) printf("Flag ACK = 0\n");
+	else printf("Flag ACK = 1\n");
+
+	return;
+}
+
+
+void analizar_UDP(const struct pcap_pkthdr *hdr, const uint8_t *pack){
+	uint16_t *paux;
+	uint16_t aux16;
+
+	printf("  Cabecera UDP:\n");
+
+	/*Puerto origen*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Puerto origen = %"PRIu16"\n", aux16);
+	if(sport_filter != 0 && aux16 != sport_filter){
+		printf("El puerto origen no pasa el filtro, no se mostraran mas campos\n");
+		return;
+	}
+
+	pack += 2;/*Antes de puerto destino*/
+	/*Puerto destino*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Puerto destino = %"PRIu16"\n", aux16);
+	if(dport_filter != 0 && aux16 != dport_filter){
+		printf("El puerto destino no pasa el filtro, no se mostraran mas campos\n");
+		return;
+	}
+
+	pack += 2;/*Antes de longitud*/
+	paux = (uint16_t *)pack;
+	aux16 = ntohs(paux[0]);
+	printf("Longitud UDP = %"PRIu16"",aux16);
+	return;
 }
