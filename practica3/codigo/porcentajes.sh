136dff47ba154bb56a126998b1676d4a67d1e125
RodrigoDePool <rodrigodp05@gmail.com>
2017-11-06 21:42:17 +0100

    "auto-pushed"

--- practica3/codigo/porcentajes.sh
+++ practica3/codigo/porcentajes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$(tshark -r traza.pcap -T fields -e eth.type -e vlan.ethype -e ip.proto > tipos) 
+$(tshark -r traza.pcap -E separator=: -T fields -e eth.type -e vlan.etype -e ip.proto > tipos)
 total=$(cat tipos | wc -l)
 $(cat tipos | grep 0800 > tiposIP)
 
@@ -8,4 +8,3 @@ paquetesIP=$(cat tiposIP | wc -l)
 paquetesUDP=$(cat tiposIP | grep 17 | wc -l)
 paquetesTCP=$(cat tiposIP | grep 6 | wc -l)
 
-printf "Porcentaje NO IP: %f\n" "$paquetesIP*100/$total"
---
+++ practica3/codigo/porcentajes2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+tshark -r traza.pcap -E separator=: -T fields -e eth.type -e vlan.etype -e ip.proto > tipos
+
+awk 'BEGIN{ FS=":"; lineas_totales=0; lineas_ip=0; lineas_udp=0; lineas_tcp=0; }
+ {
+	lineas_totales = lineas_totales + 1;
+
+	if( $1 == "0x00000800" || $2 == "0x00000800" ){
+		lineas_ip = lineas_ip + 1;
+
+		if( $3 == "6")
+			lineas_tcp = lineas_tcp + 1;
+		else if( $3 == "17" )
+			lineas_udp = lineas_udp + 1;
+	}
+} END{
+	print "No IP\t", 100 - lineas_ip*100/lineas_totales, "%";
+	print "IP\t", lineas_ip*100/lineas_totales, "%";
+
+	print "\nEntre los paquetes IP tenemos:";
+	print "UDP\t", lineas_udp*100/lineas_ip, "%";
+	print "TCP\t", lineas_tcp*100/lineas_ip, "%";
+	print "Otros\t", 100 - lineas_udp*100/lineas_ip - lineas_tcp*100/lineas_ip, "%";
+}' tipos
