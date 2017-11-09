c0f4357904a8fe457db3067452a28d696f39a028
RodrigoDePool <rodrigodp05@gmail.com>
2017-11-09 19:58:34 +0100

    "auto-pushed"

--- practica3/codigo/pruebaCDF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Script para probar funcionamineto crearCDF.c
-
-$(tshark -r traza.pcap -E separator=: -T fields -e frame.cap_len > ./data/paquetes_tamanios)
-
-$(./crearCDF ./data/paquetes_tamanios ./data/salida)
-
