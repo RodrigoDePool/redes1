471c8c1d7985132d50e4229c457d61f3c5380b40
luciaasen <luciaasen@gmail.com>
2017-11-09 14:41:15 +0100

    viva la vida

--- practica3/codigo/scriptTop.sh
+++ practica3/codigo/scriptTop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#Input: $1 = ip/tcp, $2=src/dst
+#Este script devuelve por terminal el top 10 decidido segun los argumentos de entrada
+#Adicionalmente, genera un fichero en data/top con los datos asociados a ese top 10 (direccion numpaquetes numbytes).
+#El nombre del fichero es autodescriptivo: tcp.dstport.paquetes guarda el top 10 en paquetes de puertos tcp destino
+
+#Input: $1 = ip/tcp/udp, $2=src/dst $3=paquetes/bytes
 if [ "$1" = "ip" ] 
 then
     LAYER="ip"
@@ -44,7 +48,6 @@ else
     echo "Argumento erroneo: tcp/ip/udp src/dst paquetes/bytes"
     exit -1
 fi
-#Input: $3 = paquetes/bytes
 if [ "$3" = "paquetes" ]
 then
     K=2
@@ -56,6 +59,9 @@ else
     exit -1
 fi
 
+# Si no existia anteriormente, creamos el directorio "data/top/" donde almacenar resultados
+DIR="data/top/"
+mkdir -p $DIR
 # Leemos los paquetes ip e imprimimos su ip origen y tamanio paquete
 
 tshark -r traza.pcap -E separator=, -Y $LAYER -T fields -e $WAY -e frame.len > $WAY
@@ -67,9 +73,9 @@ awk 'BEGIN{ FS=",";}{
 }
 END{
    for (key in npaquetes) {printf "%s %d paquetes %d bytes\n", key, npaquetes[key], nbytes[key] }
-}' $WAY | sort -k $K -nr | head -n 10 > "$WAY.txt"
+}' $WAY | sort -k $K -nr | head -n 10 > "$DIR$WAY.$3"
 
 rm $WAY
 
-awk 'BEGIN{}{print $1}END{}' "$WAY.txt"
+awk 'BEGIN{}{print $1}END{}' "$DIR$WAY.$3"
 
