b9bc508597163647e1ea65ca5b80bb9ed28aadf8
Cuenta provisional acceso Temporal <luciaasen@gmail.com>
2017-11-10 17:09:40 +0100

    .,m

--- practica3/codigo/practica3_analisis.sh
+++ practica3/codigo/practica3_analisis.sh
@@ -26,68 +26,68 @@ echo 'Porcentajes de paquetes por protocolo:'
 bash scripts/porcentajes.sh #Hara falta meterlo en un fichero ??
 echo ''
 
-# echo 'Top 10 IP SRC PAQUETES'
-# bash scripts/scriptTop.sh ip src paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 IP SRC PAQUETES'
+ bash scripts/scriptTop.sh ip src paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 IP SRC BYTES'
-# bash scripts/scriptTop.sh ip src bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 IP SRC BYTES'
+ bash scripts/scriptTop.sh ip src bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 IP DST PAQUETES'
-# bash scripts/scriptTop.sh ip dst paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 IP DST PAQUETES'
+ bash scripts/scriptTop.sh ip dst paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 IP SRC BYTES'
-# bash scripts/scriptTop.sh ip dst bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 IP SRC BYTES'
+ bash scripts/scriptTop.sh ip dst bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 TCP SRC PAQUETES'
-# bash scripts/scriptTop.sh tcp src paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 TCP SRC PAQUETES'
+ bash scripts/scriptTop.sh tcp src paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 TCP SRC BYTES'
-# bash scripts/scriptTop.sh tcp src bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 TCP SRC BYTES'
+ bash scripts/scriptTop.sh tcp src bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 TCP DST PAQUETES'
-# bash scripts/scriptTop.sh tcp dst paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 TCP DST PAQUETES'
+ bash scripts/scriptTop.sh tcp dst paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 TCP SRC BYTES'
-# bash scripts/scriptTop.sh tcp dst bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 TCP SRC BYTES'
+ bash scripts/scriptTop.sh tcp dst bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 UDP SRC PAQUETES'
-# bash scripts/scriptTop.sh udp src paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 UDP SRC PAQUETES'
+ bash scripts/scriptTop.sh udp src paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 UDP SRC BYTES'
-# bash scripts/scriptTop.sh udp src bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 UDP SRC BYTES'
+ bash scripts/scriptTop.sh udp src bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 UDP DST PAQUETES'
-# bash scripts/scriptTop.sh udp dst paquetes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 UDP DST PAQUETES'
+ bash scripts/scriptTop.sh udp dst paquetes #Hara falta meterlo en un fichero ??
+ echo ''
 
-# echo 'Top 10 UDP SRC BYTES'
-# bash scripts/scriptTop.sh udp dst bytes #Hara falta meterlo en un fichero ??
-# echo ''
+ echo 'Top 10 UDP SRC BYTES'
+ bash scripts/scriptTop.sh udp dst bytes #Hara falta meterlo en un fichero ??
+ echo ''
 
 #Creacion de ecdfs por tamano
 
-# bash scripts/ecdf_tam.sh eth src
-# echo ''
-# bash scripts/ecdf_tam.sh eth dst
-# echo ''
-# bash scripts/ecdf_tam.sh http src
-# echo ''
-# bash scripts/ecdf_tam.sh http dst
-# echo ''
-# bash scripts/ecdf_tam.sh dns src
-# echo ''
-# bash scripts/ecdf_tam.sh dns dst
-# echo ''
+ bash scripts/ecdf_tam.sh eth src
+ echo ''
+ bash scripts/ecdf_tam.sh eth dst
+ echo ''
+ bash scripts/ecdf_tam.sh http src
+ echo ''
+ bash scripts/ecdf_tam.sh http dst
+ echo ''
+ bash scripts/ecdf_tam.sh dns src
+ echo ''
+ bash scripts/ecdf_tam.sh dns dst
+ echo ''
 
 #Creacion de ecdfs por tiempo
 
