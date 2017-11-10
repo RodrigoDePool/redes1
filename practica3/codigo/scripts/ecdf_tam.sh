ec912273ae898375b76a7914729c8bda86b0dadd
Cuenta provisional acceso Temporal <luciaasen@gmail.com>
2017-11-10 15:35:44 +0100

    he muerto

--- practica3/codigo/scripts/ecdf_tam.sh
+++ practica3/codigo/scripts/ecdf_tam.sh
@@ -5,29 +5,27 @@
 
 #Input: $1 = eth/http/dns, $2 = src/dst
 
-MAC="00:11:88:CC:33:CA"
+MAC="00:11:88:cc:33:ca"
 # Si no existian, creamos dirs para graficas y data
 DATA="datos/ecdf_tam/"
 GRAF="graficas/ecdf_tam/"
 mkdir -p $DATA
 mkdir -p $GRAF
 #Probamos argumentos, establecemos filtro y nivel de filtrado
-if [ $2 != "src" ] && [ $2 != "src" ]; then echo "Argumento 2 erroneo: src/dst"; exit -1; fi 
-echo "$1 $2"
+if [ $2 != "dst" ] && [ $2 != "src" ]; then echo "Argumento 2 erroneo: src/dst"; exit -1; fi 
 if [ "$1" = "eth" ]; then
     if [ "$2" = "src" ]; then COL_PORT="14";
     elif [ "$2" = "dst" ]; then COL_PORT="13";
     fi
     COL_TAM="11"; FILTER=$MAC
 elif [ "$1" = "http" ]; then
-    echo "JAJA!"
-    if ["$2" = "src" ]; then COL_PORT="7"; echo "JAJA2";
-    elif ["$2" = "dst" ]; then COL_PORT="6"; echo "JAJA3";
+    if [ "$2" = "src" ]; then COL_PORT="7";
+    elif [ "$2" = "dst" ]; then COL_PORT="6";
     fi
     COL_TAM="10"; FILTER="80"
 elif [ "$1" = "dns" ]; then
     if [ "$2" = "src" ]; then COL_PORT="9";
-    elif [ "$2" = "dst "]; then COL_PORT="8";
+    elif [ "$2" = "dst" ]; then COL_PORT="8";
     fi
     COL_TAM="10"; FILTER="53"
 else
@@ -36,9 +34,8 @@ else
 fi
 
 #Guardamos en fichero tamanios deseados en inputs
-awk -v col_tam=$COL_TAM -v col_port=$COL_PORT -v filter=$FILTER 'BEGIN{ FS="\t"; printf "Tam %d Port %d Filter %d\n", col_tam, col_port, filter;}
-{   #printf "HOLA tam %s port %s filter %s\n", $col_tam, $col_port, filter
-    printf "%s columna %s contenido %s\n", $7, col_port, $col_port
+awk -v col_tam=$COL_TAM -v col_port=$COL_PORT -v filter=$FILTER 'BEGIN{ FS="\t";}
+{
     if($col_port != null && $col_port == filter ){printf "%d\n", $col_tam; }
 }
 END{}' tipos.tshark > input.tmp
@@ -58,10 +55,10 @@ unset key
 set terminal png size 800,600
 set output "$GRAF${1}_${2}.png" 
 
-plot "$DATA${1}_$2" u 1:2 w lines
+plot "$DATA${1}_$2" u 1:2 w steps
 EOF
 
-# rm input.tmp
+ rm input.tmp
 
 echo 'Grafica '${1}'_'${2}'.png creada en el directorio $GRAF'
 
