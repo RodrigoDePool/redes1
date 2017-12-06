424ab317680745c8a3ffb053f8f53b918366d95b
RodrigoDePool <rodrigodp05@gmail.com>
2017-12-06 18:25:10 +0100

    "auto-pushed"

--- practica4/codigo/practica4.c
+++ practica4/codigo/practica4.c
@@ -487,7 +487,7 @@ uint8_t moduloETH(uint8_t* datagrama, uint64_t longitud, uint16_t* pila_protocol
     uint8_t trama[ETH_FRAME_MAX]={0};
     uint32_t pos = 0;
     uint64_t mac; /*No hay de 48 :( */
-    uint16_t protocolo_superior=pila_protocolos[1];
+    uint16_t protocolo_superior=pila_protocolos[0];
     Parametros ethdatos = *((Parametros *)parametros);
     
     struct pcap_pkthdr hdr;
@@ -516,7 +516,6 @@ uint8_t moduloETH(uint8_t* datagrama, uint64_t longitud, uint16_t* pila_protocol
     pos += ETH_ALEN;
 
     /*Aniadimos tipo ethernet*/
-    printf("%d\n",protocolo_superior);
     memcpy(trama + pos, &protocolo_superior, sizeof(uint16_t));
     pos += sizeof(uint16_t);
     /*Aniadimos datagrama*/
